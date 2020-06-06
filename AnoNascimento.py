70a0537bf3710841ce3b07ab85f2a7b828ee57d8
WilliTeixeira <williTeixeira2000@msn.com>
2020-06-06 20:10:10 -0300

    'oi'

---
+++ AnoNascimento.py
@@ -0,0 +1,9 @@
+import os
+os.system("clear") or None
+
+print("Este progama informa o ano do seu\nnascimento")
+ano   = int(input ("Qual o ano atual? "))
+idade = int(input ("Qual a sua idade? "))
+resul = ano - idade
+print("Você nasceu em", resul)
+print("Você nasceu em", resul)
